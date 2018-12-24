9e56051df37c9bd666810050c8165144e7a990d5
Cassio Colello <cassiocolello@yahoo.com.br>
2018-12-24 18:13:58 -0200

    Versão inicial

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
+
+
+
+// var http = require('http');
+
+// var server = http.createServer(function(request, response) {
+
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
+
+// });
+
+// var port = process.env.PORT || 1337;
+// server.listen(port);
+
+// console.log("Server running at http://localhost:%d", port);
